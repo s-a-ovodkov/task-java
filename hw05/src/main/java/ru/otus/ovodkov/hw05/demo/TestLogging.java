c816e96949befb3659502fe5d54d0d7e1fa9ee06
Sergey Ovodkov <ovodkov.s.a@gmail.com>
2020-05-05 02:11:37 +0300

    Добавлены класс и интерфейс используемые для демонстрации

---
+++ hw05/src/main/java/ru/otus/ovodkov/hw05/demo/TestLogging.java
@@ -0,0 +1,16 @@
+package ru.otus.ovodkov.hw05.demo;
+
+/**
+ *
+ * @author Ovodkov Sergey
+ * created on 05.05.2020
+ */
+public interface TestLogging {
+    void Test1(int i);
+    void Test2(int i);
+    void Test3(int i);
+    void Test4(int i);
+    void Test5(int i, String str);
+    void Test6(int i);
+    void Test7(int i);
+}
